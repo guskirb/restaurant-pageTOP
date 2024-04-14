d81c6823ec9a4182bc67e3cee603615460289e55
guskirb <anguskirby1998@gmail.com>
2024-04-14 03:33:51 +0100

    Fix logos not showing

--- bundle.js
+++ bundle.js
@@ -42,6 +42,11 @@ ___CSS_LOADER_EXPORT___.push([module.id, `@font-face {
   font-style: normal;
 }
 
+:root {
+  --accent-color: #0c0100;
+  --background-color: #fcf7ec;
+}
+
 body {
   margin: 0;
   box-sizing: border-box;
@@ -49,6 +54,7 @@ body {
   display: flex;
   background-size: cover;
   background-position: center;
+  background-color: var(--background-color);
 }
 
 header {
@@ -57,12 +63,12 @@ header {
   justify-content: space-between;
   align-items: center;
   font-family: "Sedan", sans-serif;
-  color: #ffffff;
+  color: var(--background-color);
   height: 100vh;
-  width: max(280px, 20vw);
+  min-width: 20vw;
   text-align: center;
   z-index: 1;
-  background-color: #0c0100;
+  background-color: var(--accent-color);
 }
 
 header > h1 {
@@ -84,15 +90,15 @@ nav > button {
   height: 5vh;
   border: none;
   background: none;
-  color: #ffffff;
+  color: var(--background-color);
   font-size: 1.2em;
   text-align: center;
   font-family: "Sedan", sans-serif;
-  border: solid 1px #0c0100;
+  border: solid 1px var(--accent-color);
 }
 
 nav > button:hover {
-  border-bottom: solid 1px white;
+  border-bottom: solid 1px var(--background-color);
 }
 
 .headerBottom {
@@ -110,6 +116,8 @@ nav > button:hover {
   grid-template-columns: 1fr 1fr 1fr;
   gap: 1.2vw;
   margin: 1.2vw;
+  font-family: "Sedan", sans-serif;
+  min-width: 77.8vw;
 }
 
 #content > img {
@@ -117,7 +125,39 @@ nav > button:hover {
   height: 95vh;
   object-fit: cover;
 }
-`, "",{"version":3,"sources":["webpack://./src/style.css"],"names":[],"mappings":"AAAA;EACE,2BAA2B;EAC3B,4CAAqC;EACrC,gBAAgB;EAChB,kBAAkB;AACpB;;AAEA;EACE,oBAAoB;EACpB,4CAA8B;EAC9B,gBAAgB;EAChB,kBAAkB;AACpB;;AAEA;EACE,SAAS;EACT,sBAAsB;EACtB,iBAAiB;EACjB,aAAa;EACb,sBAAsB;EACtB,2BAA2B;AAC7B;;AAEA;EACE,aAAa;EACb,iBAAiB;EACjB,8BAA8B;EAC9B,mBAAmB;EACnB,gCAAgC;EAChC,cAAc;EACd,aAAa;EACb,uBAAuB;EACvB,kBAAkB;EAClB,UAAU;EACV,yBAAyB;AAC3B;;AAEA;EACE,cAAc;EACd,gBAAgB;EAChB,uCAAuC;AACzC;;AAEA;EACE,aAAa;EACb,WAAW;EACX,iBAAiB;EACjB,QAAQ;EACR,mBAAmB;AACrB;;AAEA;EACE,UAAU;EACV,WAAW;EACX,YAAY;EACZ,gBAAgB;EAChB,cAAc;EACd,gBAAgB;EAChB,kBAAkB;EAClB,gCAAgC;EAChC,yBAAyB;AAC3B;;AAEA;EACE,8BAA8B;AAChC;;AAEA;EACE,mBAAmB;EACnB,gBAAgB;AAClB;;AAEA;EACE,YAAY;AACd;;AAEA;EACE,aAAa;EACb,uBAAuB;EACvB,kCAAkC;EAClC,UAAU;EACV,aAAa;AACf;;AAEA;EACE,WAAW;EACX,YAAY;EACZ,iBAAiB;AACnB","sourcesContent":["@font-face {\n  font-family: \"Callingstone\";\n  src: url(/Resources/Callingstone.ttf);\n  font-weight: 500;\n  font-style: normal;\n}\n\n@font-face {\n  font-family: \"Sedan\";\n  src: url(/Resources/Sedan.ttf);\n  font-weight: 500;\n  font-style: normal;\n}\n\nbody {\n  margin: 0;\n  box-sizing: border-box;\n  min-height: 100vh;\n  display: flex;\n  background-size: cover;\n  background-position: center;\n}\n\nheader {\n  display: flex;\n  flex-flow: column;\n  justify-content: space-between;\n  align-items: center;\n  font-family: \"Sedan\", sans-serif;\n  color: #ffffff;\n  height: 100vh;\n  width: max(280px, 20vw);\n  text-align: center;\n  z-index: 1;\n  background-color: #0c0100;\n}\n\nheader > h1 {\n  font-size: 2em;\n  padding-top: 5vh;\n  font-family: \"Callingstone\", sans-serif;\n}\n\nnav {\n  display: flex;\n  width: 12vw;\n  flex-flow: column;\n  gap: 3vw;\n  align-items: center;\n}\n\nnav > button {\n  width: 5vw;\n  height: 5vh;\n  border: none;\n  background: none;\n  color: #ffffff;\n  font-size: 1.2em;\n  text-align: center;\n  font-family: \"Sedan\", sans-serif;\n  border: solid 1px #0c0100;\n}\n\nnav > button:hover {\n  border-bottom: solid 1px white;\n}\n\n.headerBottom {\n  padding-bottom: 5vh;\n  font-size: 1.1em;\n}\n\n.headerBottom > img {\n  height: 35px;\n}\n\n#content {\n  display: grid;\n  grid-template-rows: 1fr;\n  grid-template-columns: 1fr 1fr 1fr;\n  gap: 1.2vw;\n  margin: 1.2vw;\n}\n\n#content > img {\n  width: 25vw;\n  height: 95vh;\n  object-fit: cover;\n}\n"],"sourceRoot":""}]);
+
+#content > div {
+  text-align: center;
+  border: solid 2px var(--accent-color);
+  padding: 20px;
+  color: var(--accent-color);
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+#content h2 {
+  font-size: 2em;
+}
+
+#content h3 {
+  font-size: 1.3em;
+}
+
+#content p {
+  font-size: 1em;
+}
+
+#content > div > div {
+  text-align: center;
+  border: solid 1px var(--accent-color);
+  padding: 20px;
+  color: var(--accent-color);
+  list-style: none;
+  width: 20vw;
+}
+`, "",{"version":3,"sources":["webpack://./src/style.css"],"names":[],"mappings":"AAAA;EACE,2BAA2B;EAC3B,4CAAqC;EACrC,gBAAgB;EAChB,kBAAkB;AACpB;;AAEA;EACE,oBAAoB;EACpB,4CAA8B;EAC9B,gBAAgB;EAChB,kBAAkB;AACpB;;AAEA;EACE,uBAAuB;EACvB,2BAA2B;AAC7B;;AAEA;EACE,SAAS;EACT,sBAAsB;EACtB,iBAAiB;EACjB,aAAa;EACb,sBAAsB;EACtB,2BAA2B;EAC3B,yCAAyC;AAC3C;;AAEA;EACE,aAAa;EACb,iBAAiB;EACjB,8BAA8B;EAC9B,mBAAmB;EACnB,gCAAgC;EAChC,8BAA8B;EAC9B,aAAa;EACb,eAAe;EACf,kBAAkB;EAClB,UAAU;EACV,qCAAqC;AACvC;;AAEA;EACE,cAAc;EACd,gBAAgB;EAChB,uCAAuC;AACzC;;AAEA;EACE,aAAa;EACb,WAAW;EACX,iBAAiB;EACjB,QAAQ;EACR,mBAAmB;AACrB;;AAEA;EACE,UAAU;EACV,WAAW;EACX,YAAY;EACZ,gBAAgB;EAChB,8BAA8B;EAC9B,gBAAgB;EAChB,kBAAkB;EAClB,gCAAgC;EAChC,qCAAqC;AACvC;;AAEA;EACE,gDAAgD;AAClD;;AAEA;EACE,mBAAmB;EACnB,gBAAgB;AAClB;;AAEA;EACE,YAAY;AACd;;AAEA;EACE,aAAa;EACb,uBAAuB;EACvB,kCAAkC;EAClC,UAAU;EACV,aAAa;EACb,gCAAgC;EAChC,iBAAiB;AACnB;;AAEA;EACE,WAAW;EACX,YAAY;EACZ,iBAAiB;AACnB;;AAEA;EACE,kBAAkB;EAClB,qCAAqC;EACrC,aAAa;EACb,0BAA0B;EAC1B,aAAa;EACb,iBAAiB;EACjB,8BAA8B;EAC9B,mBAAmB;AACrB;;AAEA;EACE,cAAc;AAChB;;AAEA;EACE,gBAAgB;AAClB;;AAEA;EACE,cAAc;AAChB;;AAEA;EACE,kBAAkB;EAClB,qCAAqC;EACrC,aAAa;EACb,0BAA0B;EAC1B,gBAAgB;EAChB,WAAW;AACb","sourcesContent":["@font-face {\n  font-family: \"Callingstone\";\n  src: url(/Resources/Callingstone.ttf);\n  font-weight: 500;\n  font-style: normal;\n}\n\n@font-face {\n  font-family: \"Sedan\";\n  src: url(/Resources/Sedan.ttf);\n  font-weight: 500;\n  font-style: normal;\n}\n\n:root {\n  --accent-color: #0c0100;\n  --background-color: #fcf7ec;\n}\n\nbody {\n  margin: 0;\n  box-sizing: border-box;\n  min-height: 100vh;\n  display: flex;\n  background-size: cover;\n  background-position: center;\n  background-color: var(--background-color);\n}\n\nheader {\n  display: flex;\n  flex-flow: column;\n  justify-content: space-between;\n  align-items: center;\n  font-family: \"Sedan\", sans-serif;\n  color: var(--background-color);\n  height: 100vh;\n  min-width: 20vw;\n  text-align: center;\n  z-index: 1;\n  background-color: var(--accent-color);\n}\n\nheader > h1 {\n  font-size: 2em;\n  padding-top: 5vh;\n  font-family: \"Callingstone\", sans-serif;\n}\n\nnav {\n  display: flex;\n  width: 12vw;\n  flex-flow: column;\n  gap: 3vw;\n  align-items: center;\n}\n\nnav > button {\n  width: 5vw;\n  height: 5vh;\n  border: none;\n  background: none;\n  color: var(--background-color);\n  font-size: 1.2em;\n  text-align: center;\n  font-family: \"Sedan\", sans-serif;\n  border: solid 1px var(--accent-color);\n}\n\nnav > button:hover {\n  border-bottom: solid 1px var(--background-color);\n}\n\n.headerBottom {\n  padding-bottom: 5vh;\n  font-size: 1.1em;\n}\n\n.headerBottom > img {\n  height: 35px;\n}\n\n#content {\n  display: grid;\n  grid-template-rows: 1fr;\n  grid-template-columns: 1fr 1fr 1fr;\n  gap: 1.2vw;\n  margin: 1.2vw;\n  font-family: \"Sedan\", sans-serif;\n  min-width: 77.8vw;\n}\n\n#content > img {\n  width: 25vw;\n  height: 95vh;\n  object-fit: cover;\n}\n\n#content > div {\n  text-align: center;\n  border: solid 2px var(--accent-color);\n  padding: 20px;\n  color: var(--accent-color);\n  display: flex;\n  flex-flow: column;\n  justify-content: space-between;\n  align-items: center;\n}\n\n#content h2 {\n  font-size: 2em;\n}\n\n#content h3 {\n  font-size: 1.3em;\n}\n\n#content p {\n  font-size: 1em;\n}\n\n#content > div > div {\n  text-align: center;\n  border: solid 1px var(--accent-color);\n  padding: 20px;\n  color: var(--accent-color);\n  list-style: none;\n  width: 20vw;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -597,6 +637,63 @@ module.exports = styleTagTransform;
 
 /***/ }),
 
+/***/ "./src/about.js":
+/*!**********************!*\
+  !*** ./src/about.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   aboutLoad: () => (/* binding */ aboutLoad)
+/* harmony export */ });
+/* harmony import */ var _Resources_food2_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Resources/food2.jpg */ "./Resources/food2.jpg");
+/* harmony import */ var _Resources_food3_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../Resources/food3.jpg */ "./Resources/food3.jpg");
+
+
+
+const container = document.getElementById("content")
+const textDiv = document.createElement("div")
+const textHeader = document.createElement("h2")
+const textSubHeader = document.createElement("h3")
+const textBox = document.createElement("p")
+const openingTimes = document.createElement("div")
+
+textHeader.textContent = "Welcome to La Restaurant!"
+textSubHeader.textContent = "Providing a luxury dining experience for over 20 years!"
+textBox.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pellentesque enim risus, nec rhoncus magna vulputate a. Duis tincidunt accumsan diam, nec commodo ligula pulvinar in. Proin augue orci, vestibulum vel urna vitae, bibendum laoreet diam. Quisque sed lobortis tortor. Donec non interdum purus. Donec eget erat sed lectus semper molestie. Fusce efficitur eros metus, sed ultrices turpis facilisis a. Ut luctus orci magna, ac faucibus ligula tristique sit amet. Mauris ultrices quis quam sit amet bibendum. Sed blandit augue eget mauris feugiat cursus. In hac habitasse platea dictumst. Pellentesque volutpat eleifend ligula, a elementum sapien pulvinar sed. Sed dapibus libero nec arcu sodales, et consequat dolor sollicitudin. Praesent laoreet nulla ac semper tincidunt."
+
+function aboutLoad() {
+    while (openingTimes.firstChild) {
+        openingTimes.removeChild(openingTimes.lastChild);
+    };
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+    container.appendChild(textDiv)
+    textDiv.appendChild(textHeader)
+    textDiv.appendChild(textSubHeader)
+    textDiv.appendChild(textBox)
+    textDiv.appendChild(openingTimes)
+    openingTimes.appendChild(document.createElement("li")).textContent = "Brunch"
+    openingTimes.appendChild(document.createElement("li")).textContent = "Saturday + Sunday: 11 a.m. - 2:30 p.m."
+    openingTimes.appendChild(document.createElement("li")).textContent = "Dinner"
+    openingTimes.appendChild(document.createElement("li")).textContent = "Sunday: 5 - 9 p.m."
+    openingTimes.appendChild(document.createElement("li")).textContent = "Monday - Thursday: 5 - 10 p.m."
+    openingTimes.appendChild(document.createElement("li")).textContent = "Friday + Saturday: 5 - 11 p.m."
+    openingTimes.appendChild(document.createElement("li")).textContent = "Friday + Saturday: 5 - 11 p.m."
+    openingTimes.appendChild(document.createElement("li")).textContent = "Happy Hour"
+    openingTimes.appendChild(document.createElement("li")).textContent = "Monday - Friday: 5 - 7 p.m."
+    let pic2 = document.createElement("img");
+    let pic3 = document.createElement("img");
+    pic2.src = _Resources_food2_jpg__WEBPACK_IMPORTED_MODULE_0__;
+    pic3.src = _Resources_food3_jpg__WEBPACK_IMPORTED_MODULE_1__;
+    container.appendChild(pic2)
+    container.appendChild(pic3)
+}
+
+/***/ }),
+
 /***/ "./src/home.js":
 /*!*********************!*\
   !*** ./src/home.js ***!
@@ -616,6 +713,10 @@ __webpack_require__.r(__webpack_exports__);
 const container = document.getElementById("content")
 
 function homeLoad() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+
     let pic1 = document.createElement("img");
     let pic2 = document.createElement("img");
     let pic3 = document.createElement("img");
@@ -628,6 +729,69 @@ function homeLoad() {
     container.appendChild(pic2);
     container.appendChild(pic3);
 
+}
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   menuLoad: () => (/* binding */ menuLoad)
+/* harmony export */ });
+/* harmony import */ var _Resources_food3_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Resources/food3.jpg */ "./Resources/food3.jpg");
+
+
+const container = document.getElementById("content")
+const textDiv1 = document.createElement("div")
+const textDiv2 = document.createElement("div")
+
+function menuLoad() {
+    while (textDiv1.firstChild) {
+        textDiv1.removeChild(textDiv1.lastChild);
+    };
+    while (textDiv2.firstChild) {
+        textDiv2.removeChild(textDiv2.lastChild);
+    };
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+    container.appendChild(textDiv1)
+    container.appendChild(textDiv2)
+
+    textDiv1.appendChild(document.createElement("h2")).textContent = "MENU"
+    textDiv1.appendChild(document.createElement("h3")).textContent = "Main Course"
+    textDiv1.appendChild(document.createElement("h4")).textContent = "Potato Gnocchi"
+    textDiv1.appendChild(document.createElement("p")).textContent = "Purple sprouting broccoli, black garlic, Pecorino (v)"
+    textDiv1.appendChild(document.createElement("h4")).textContent = "Pork Chop"
+    textDiv1.appendChild(document.createElement("p")).textContent = "Cime di rapa, Belper Knolle"
+    textDiv1.appendChild(document.createElement("h4")).textContent = "Steak au Poivre"
+    textDiv1.appendChild(document.createElement("p")).textContent = "Peppercorn sauce, truffle"
+    textDiv1.appendChild(document.createElement("h3")).textContent = "Sides"
+    textDiv1.appendChild(document.createElement("h5")).textContent = "Mashed Potatoes (v)"
+    textDiv1.appendChild(document.createElement("h5")).textContent = "French Fries (v)"
+    textDiv1.appendChild(document.createElement("h5")).textContent = "Glazed Garden Carrots, Carrot Purée (vg)"
+
+    textDiv2.appendChild(document.createElement("h2")).textContent = "MENU"
+    textDiv2.appendChild(document.createElement("h3")).textContent = "Desserts"
+    textDiv2.appendChild(document.createElement("h4")).textContent = "Chocolate Soufflé Tart"
+    textDiv2.appendChild(document.createElement("p")).textContent = "Cocoa nib ice cream"
+    textDiv2.appendChild(document.createElement("h4")).textContent = "Blood Orange Baked Alaska"
+    textDiv2.appendChild(document.createElement("p")).textContent = "Pomegranate, Grand Marnier"
+    textDiv2.appendChild(document.createElement("h4")).textContent = "Homemade Sorbet or Ice Cream"
+    textDiv2.appendChild(document.createElement("p")).textContent = "Vanilla, cocoa nib, rhubarb, blood orange & pomegrante, Marsala, rum"
+    textDiv2.appendChild(document.createElement("h3")).textContent = "Coffee"
+    textDiv2.appendChild(document.createElement("h5")).textContent = "Machiatto"
+    textDiv2.appendChild(document.createElement("h5")).textContent = "Espresso/Double Espresso"
+    textDiv2.appendChild(document.createElement("h5")).textContent = "Cappucino"
+
+    let pic = document.createElement("img");
+    pic.src = _Resources_food3_jpg__WEBPACK_IMPORTED_MODULE_0__;
+    container.appendChild(pic)
 }
 
 /***/ }),
@@ -649,6 +813,16 @@ module.exports = __webpack_require__.p + "e72e3a749f0383fb7b47.ttf";
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 module.exports = __webpack_require__.p + "d7fc4427c1781412445c.ttf";
+
+/***/ }),
+
+/***/ "./Resources/facebook.svg":
+/*!********************************!*\
+  !*** ./Resources/facebook.svg ***!
+  \********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "447507321d8589bea1fa.svg";
 
 /***/ }),
 
@@ -679,6 +853,26 @@ module.exports = __webpack_require__.p + "4ed23c61baaebc92222b.jpg";
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 module.exports = __webpack_require__.p + "457e447721227da5eaf3.jpg";
+
+/***/ }),
+
+/***/ "./Resources/instagram.svg":
+/*!*********************************!*\
+  !*** ./Resources/instagram.svg ***!
+  \*********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "26ad17adbefaa914861a.svg";
+
+/***/ }),
+
+/***/ "./Resources/twitter.svg":
+/*!*******************************!*\
+  !*** ./Resources/twitter.svg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "0f7406768dabc17cda96.svg";
 
 /***/ })
 
@@ -827,13 +1021,43 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ "./src/home.js");
-/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _about__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./about */ "./src/about.js");
+/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./menu */ "./src/menu.js");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _Resources_instagram_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../Resources/instagram.svg */ "./Resources/instagram.svg");
+/* harmony import */ var _Resources_facebook_svg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../Resources/facebook.svg */ "./Resources/facebook.svg");
+/* harmony import */ var _Resources_twitter_svg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../Resources/twitter.svg */ "./Resources/twitter.svg");
 
 
 
-(0,_home__WEBPACK_IMPORTED_MODULE_0__.homeLoad)();
 
-console.log('hi');
+
+
+
+
+const homeButton = document.getElementById("home")
+const aboutButton = document.getElementById("about")
+const menuButton = document.getElementById("menu")
+const img1 = new Image();
+const img2 = new Image();
+const img3 = new Image();
+const footer = document.querySelector(".headerBottom")
+
+img1.src = /* non-default import from non-esm module */undefined
+img2.src = /* non-default import from non-esm module */undefined
+img3.src = /* non-default import from non-esm module */undefined
+
+footer.appendChild(img1);
+footer.appendChild(img2);
+footer.appendChild(img3);
+
+(0,_home__WEBPACK_IMPORTED_MODULE_0__.homeLoad)()
+
+aboutButton.addEventListener("click", _about__WEBPACK_IMPORTED_MODULE_1__.aboutLoad);
+homeButton.addEventListener("click", _home__WEBPACK_IMPORTED_MODULE_0__.homeLoad);
+menuButton.addEventListener("click", _menu__WEBPACK_IMPORTED_MODULE_2__.menuLoad);
+
+
 
 
 })();
